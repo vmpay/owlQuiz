42d9db8ad11f55a2daca994e94bb735aa20093af
vmpay <vereszp@gmail.com>
2018-04-04 23:00:26 +0200

    Account initial commit

--- app/src/main/java/eu/vmpay/owlquiz/activities/timer/MainActivity.kt
+++ app/src/main/java/eu/vmpay/owlquiz/activities/timer/MainActivity.kt
@@ -2,20 +2,23 @@ package eu.vmpay.owlquiz.activities.timer
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
 import android.view.MenuItem
 import eu.vmpay.owlquiz.R
 import eu.vmpay.owlquiz.activities.pref.PrefActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {//, NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-    }
+//    }
 
 
 //        fab.setOnClickListener { view ->
@@ -23,27 +26,27 @@ class MainActivity : AppCompatActivity() {//, NavigationView.OnNavigationItemSel
 //                    .setAction("Action", null).show()
 //        }
 
-//        val toggle = ActionBarDrawerToggle(
-//                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-//        drawer_layout.addDrawerListener(toggle)
-//        toggle.syncState()
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
 
-//        nav_view.setNavigationItemSelectedListener(this)
-//    }
-
-//    override fun onBackPressed() {
-//        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-//            drawer_layout.closeDrawer(GravityCompat.START)
-//        } else {
-//            super.onBackPressed()
-//        }
-//    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
+        nav_view.setNavigationItemSelectedListener(this)
     }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.main, menu)
+//        return true
+//    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
@@ -59,30 +62,23 @@ class MainActivity : AppCompatActivity() {//, NavigationView.OnNavigationItemSel
         }
     }
 
-//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-//        // Handle navigation view item clicks here.
-//        when (item.itemId) {
-//            R.id.nav_camera -> {
-//                // Handle the camera action
-//            }
-//            R.id.nav_gallery -> {
-//
-//            }
-//            R.id.nav_slideshow -> {
-//
-//            }
-//            R.id.nav_manage -> {
-//
-//            }
-//            R.id.nav_share -> {
-//
-//            }
-//            R.id.nav_send -> {
-//
-//            }
-//        }
-//
-//        drawer_layout.closeDrawer(GravityCompat.START)
-//        return true
-//    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_account -> {
+
+            }
+            R.id.nav_timer -> {
+                // Nothing to do. We're already here
+            }
+            R.id.nav_settings -> {
+                val intent = Intent(this, PrefActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
 }
