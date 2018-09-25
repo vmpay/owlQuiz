c55515035e3498a427daa8860589e29b94ee89dd
vmpay <vereszp@gmail.com>
2018-09-25 16:35:38 +0200

    Team requests implemented

--- app/src/main/java/eu/vmpay/owlquiz/repository/AppDatabase.kt
+++ app/src/main/java/eu/vmpay/owlquiz/repository/AppDatabase.kt
@@ -6,7 +6,9 @@ import android.arch.persistence.room.RoomDatabase
 /**
  * Created by Andrew on 12/04/2018.
  */
-@Database(entities = arrayOf(Player::class, PlayerRating::class), version = 1)
+@Database(entities = arrayOf(Player::class, PlayerRating::class, PlayerTeam::class, Team::class,
+        TeamRating::class),
+        version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): PlayerDao
 }
